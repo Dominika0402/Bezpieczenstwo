6210c7a675cece851869e9b0a6ab8157014a4c60
unknown <domi.rut@onet.pl>
2017-11-24 21:28:18 +0100

    finger

--- app/src/main/java/com/example/dominika/password_bezpieczenstwo/Password.java
+++ app/src/main/java/com/example/dominika/password_bezpieczenstwo/Password.java
@@ -13,13 +13,17 @@ import android.widget.Toast;
 import java.security.Key;
 import java.security.MessageDigest;
 import java.security.SignatureException;
+import java.util.regex.Pattern;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
+import me.aflak.libraries.FingerprintCallback;
+import me.aflak.libraries.FingerprintDialog;
+
 
 //STRONA STARTOWA
-public class Password extends AppCompatActivity {
+public class Password extends AppCompatActivity implements View.OnClickListener, FingerprintCallback {
 
     Context context;
 
@@ -32,6 +36,7 @@ public class Password extends AppCompatActivity {
         context = getApplicationContext();
         Button password = (Button) findViewById(R.id.password_B);
         final EditText password_et = (EditText) findViewById(R.id.password_ET);
+        findViewById(R.id.fingerprint).setOnClickListener(this);
 
 
         password.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +45,7 @@ public class Password extends AppCompatActivity {
                 SharedPreferences preferences = getApplicationContext().getSharedPreferences("preferences_name",
                         getApplicationContext().MODE_PRIVATE);
                 String value = preferences.getString("password", "");
+
                 if (value.equals(sha256(password_et.getText().toString())))
                 {
                     Intent intent = new Intent(Password.this, Option.class);
@@ -80,5 +86,43 @@ public class Password extends AppCompatActivity {
         } catch(Exception ex){
             throw new RuntimeException(ex);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(FingerprintDialog.isAvailable(this)) {
+            FingerprintDialog.initialize(this)
+                    .title("LOGOWANIE ZA POMOCA ODCISKU PALCA")
+                    .message("PRZYSUN PALEC DO CZYTNIKA")
+                    .callback(this)
+                    .show();
+        }
+    }
+
+    @Override
+    public void onAuthenticationSuccess() {
+        Toast.makeText(Password.this,"ZALOGOWANO",
+                Toast.LENGTH_SHORT).show();
+
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("preferences_name",
+                getApplicationContext().MODE_PRIVATE);
+        String value = preferences.getString("password", "");
+        String message = preferences.getString("message", "");
+
+        if (value.equals("") && message.equals(""))
+        {
+            Intent intent = new Intent(Password.this, WriteMessage.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(Password.this, Option.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onAuthenticationCancel() {
+        Toast.makeText(Password.this," BLAD",
+                Toast.LENGTH_SHORT).show();
     }
 }
